f997b54c37d449001305bbdca0125ccf6671c869
willtuker <william.soro@hotmail.it>
2022-12-09 17:58:17 +0100

    Day 6 solutions

---
+++ Day6/solver_part2.py
@@ -0,0 +1,39 @@
+'''
+--- Day 6: Tuning Trouble ---
+--- Part Two ---
+
+Your device's communication system is correctly detecting packets, but still isn't working. It looks like it also needs to look for messages.
+
+A start-of-message marker is just like a start-of-packet marker, except it consists of 14 distinct characters rather than 4.
+
+Here are the first positions of start-of-message markers for all of the above examples:
+
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+    bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+    nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+    nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+    zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+
+How many characters need to be processed before the first start-of-message marker is detected?
+
+'''
+
+def hasOnlyUniqueChar(strVal):
+    onlyUnique = True
+    for char in strVal:
+        if strVal.count(char) > 1:
+            onlyUnique = False
+            return onlyUnique
+
+    return onlyUnique
+
+f = open("input.txt","r")
+line = f.readline()
+
+packetLenght = 14
+
+for i in range(len(line) - packetLenght):
+    subString = line[i:packetLenght+i]
+    if hasOnlyUniqueChar(subString):
+        print(str(i+packetLenght) + " : " + subString)
+        break
